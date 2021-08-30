ea01e64e031e35d0944156e4436bba9176b1ef7b
SahilMehar <68806020+SahilMehar@users.noreply.github.com>
2021-08-30 17:55:11 +0530

    Create Solution2.cpp
    Given an unsorted array of integers, design an algorithm and implement a program to sort this
    array using selection sort. Your program should also find number of comparisons and number of
    swaps required.

---
+++ Week 3/Solution2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+void selectionSort(int a[], int n)
+{
+    int swaps=0; bool flag=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                flag=1;;
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        swaps++;
+
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"comparisons = "<<n*(n-1)/2<<endl;
+    cout<<"swaps = "<<swaps<<endl;
+    return ;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t)
+    {
+        
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        selectionSort(a,n);
+        t--;
+    }
+}
